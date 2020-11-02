040d3971ebf8012b5c150c7b1e4ecf46e4cec5d9
Gabriellji <gabriellji@yahoo.com>
2020-11-02 12:58:56 +0300

    feat: add basic page layout

---
+++ index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+const wilders = {
+    first: 'IOAN',
+    second: 'JENIYA'
+}
+ 
+console.log(chalk.blue(wilders.first));
+
+console.log(chalk.green(wilders.second));
